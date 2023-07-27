51f74d6b2ef39fd00ed4fd29c0ade8e465779172
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-07-27 18:59:54 +0300

    Add project files.

---
+++ UdemySignalR.APIProject/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace UdemySignalR.APIProject.Hubs
+{
+    public class MyHub : Hub
+    {
+
+        public static List<string> Names { get; set; } = new List<string>();
+        public async Task SendMessage(string name)
+        {
+            Names.Add(name);//api ayakta kaldığı sürece listeler içinde tutulur
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+
+        public async Task GetMessages() //clientler GetNames metodunu çağırdığında,ReceiveNames metodu tetiklenir ve Names parametresi gönderilir
+        {
+            await Clients.All.SendAsync("ReceiveName", Names);
+        }
+    }
+}
